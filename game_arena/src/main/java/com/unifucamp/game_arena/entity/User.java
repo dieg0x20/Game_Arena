7aeec593cdee0ce192cc17622242739c883fe8d0
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-19 14:38:33 -0300

    fix: adicionado a anotação @Table na classe User para mapear a tabela 'usuario'

--- game_arena/src/main/java/com/unifucamp/game_arena/entity/User.java
+++ game_arena/src/main/java/com/unifucamp/game_arena/entity/User.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
+@Table(name = "usuario")
 @Data
 @Entity
 public class User {
