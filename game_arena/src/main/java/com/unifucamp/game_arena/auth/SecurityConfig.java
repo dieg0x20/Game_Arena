e2fd296e31054377297ae713baf6d15cb30c7b6d
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-19 19:56:16 -0300

    feat: removido dependência do springdoc e desabilitado acesso ao Swagger na configuração de segurança

--- game_arena/src/main/java/com/unifucamp/game_arena/auth/SecurityConfig.java
+++ game_arena/src/main/java/com/unifucamp/game_arena/auth/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
