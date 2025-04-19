019966e1921df3775c9947a93aebac3ec8d8733d
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-19 01:21:10 -0300

    initial commit

---
+++ game_arena/src/main/java/com/unifucamp/game_arena/GameArenaApplication.java
@@ -0,0 +1,13 @@
+package com.unifucamp.game_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameArenaApplication.class, args);
+	}
+
+}
---
+++ game_arena/src/test/java/com/unifucamp/game_arena/GameArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifucamp.game_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
