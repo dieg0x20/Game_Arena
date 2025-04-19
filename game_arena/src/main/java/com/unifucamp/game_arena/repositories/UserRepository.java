7359886d48fa365bf5fd0c60c43a840cba238896
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-19 16:20:11 -0300

    feat: criado SubscriptionRepository e removido UserRepository

--- game_arena/src/main/java/com/unifucamp/game_arena/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.unifucamp.game_arena.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.unifucamp.game_arena.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
