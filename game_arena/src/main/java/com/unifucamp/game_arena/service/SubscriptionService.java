6014f780e67cb0c21ca680a9f06b6a960cc61d01
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-20 18:22:33 -0300

    refactor: melhorar a formatação e a estrutura do método findAll em SubscriptionService

--- game_arena/src/main/java/com/unifucamp/game_arena/service/SubscriptionService.java
+++ game_arena/src/main/java/com/unifucamp/game_arena/service/SubscriptionService.java
@@ -10,19 +10,33 @@ import com.unifucamp.game_arena.repositories.SubscriptionRepository;
 
 @Service
 public class SubscriptionService {
-  
-  private final SubscriptionRepository subscriptionRepository;
 
+  private final SubscriptionRepository subscriptionRepository;
 
   public SubscriptionService(SubscriptionRepository subscriptionRepository) {
     this.subscriptionRepository = subscriptionRepository;
   }
 
+  public Page<Subscription> findAll(int page, int size) {
 
-  public Page<Subscription> findAll(int page, int size){
+    if (page < 0)
+      page = 0;
+
+    if (size < 5)
+      size = 5;
+
+    if (size > 30)
+      size = 30;
 
     Pageable pageable = PageRequest.of(page, size);
-    return subscriptionRepository.findAll(pageable);
+
+    try {
+
+      return subscriptionRepository.findAll(pageable);
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
