1f803113d6b7679af47ea3aa36b6ae433fa7f038
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-20 23:32:52 -0300

    feat: Implementado método findAll na classe InscricaoService para listar inscrições paginadas

--- gamearena/src/main/java/com/unifucamp/gamearena/service/InscricaoService.java
+++ gamearena/src/main/java/com/unifucamp/gamearena/service/InscricaoService.java
@@ -1,8 +1,32 @@
 package com.unifucamp.gamearena.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.unifucamp.gamearena.dto.InscricaoResponse;
+import com.unifucamp.gamearena.repositories.InscricaoRepository;
 
 @Service
 public class InscricaoService {
 
+  private final InscricaoRepository inscricaoRepository;
+
+  public InscricaoService(InscricaoRepository inscricaoRepository) {
+    this.inscricaoRepository = inscricaoRepository;
+  }
+
+  public Page<InscricaoResponse> findAll(Pageable pageable) {
+    try {
+      return inscricaoRepository.findAll(pageable).map(i -> new InscricaoResponse(
+          i.getId(),
+          i.getNomeCompleto(),
+          i.getNickname(),
+          i.getEmail(),
+          i.isPagamento(),
+          i.getComprovantePagamento()));
+    } catch (Exception e) {
+      throw new RuntimeException("Erro ao buscar inscrições", e);
+    }
+  }
 }
