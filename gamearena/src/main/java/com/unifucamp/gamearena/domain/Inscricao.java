c16d4e3e78a71ead03defee1c4b4a93594b5b356
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-20 22:54:48 -0300

    feat: Adicionado anotações de coluna à classe Inscricao para mapeamento correto no banco de dados

--- gamearena/src/main/java/com/unifucamp/gamearena/domain/Inscricao.java
+++ gamearena/src/main/java/com/unifucamp/gamearena/domain/Inscricao.java
@@ -3,14 +3,17 @@ package com.unifucamp.gamearena.domain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "isncricoes")
 public class Inscricao {
 
   @Id
@@ -22,12 +25,16 @@ public class Inscricao {
 
   private String email;
 
+  @Column(name = "data_nascimento")
   private LocalDate dataNascimento;
 
+  @Column(name = "comprovante_pagamento")
   private String comprovantePagamento;
 
+  @Column(name = "aceitou_termos")
   private boolean aceitouTermos;
 
+  @Column(name = "data_inscricao")
   private LocalDateTime dataInscricao;
 
   private boolean ativo;
