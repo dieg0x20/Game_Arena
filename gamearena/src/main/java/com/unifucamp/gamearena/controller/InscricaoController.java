b92c55bb90bddd76800cfaf742a9795831063840
Gladyson Rhuan <gladysonr04@gmail.com>
2025-04-20 23:32:24 -0300

    feat: Implementar controlador Inscricao com método para listar inscrições paginadas

--- gamearena/src/main/java/com/unifucamp/gamearena/controller/InscricaoController.java
+++ gamearena/src/main/java/com/unifucamp/gamearena/controller/InscricaoController.java
@@ -1,8 +1,27 @@
 package com.unifucamp.gamearena.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.unifucamp.gamearena.dto.InscricaoResponse;
+import com.unifucamp.gamearena.service.InscricaoService;
 
 @RestController
 public class InscricaoController {
+
+  private final InscricaoService inscricaoService;
+
+  public InscricaoController(InscricaoService inscricaoService) {
+    this.inscricaoService = inscricaoService;
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<InscricaoResponse>> findAll(@PageableDefault(page = 0, size = 10) Pageable pageable) {
+    return ResponseEntity.ok(inscricaoService.findAll(pageable));
+  }
 
 }
