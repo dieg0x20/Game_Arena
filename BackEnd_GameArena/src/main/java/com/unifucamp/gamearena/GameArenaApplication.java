f8cd509ab5e08598e58fc3ce1da81129e6bd0f22
Gladyson Rhuan <gladysonr04@gmail.com>
2025-07-12 17:13:57 -0300

    Movendo arquivos.

--- BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/GameArenaApplication.java
+++ BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/GameArenaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GamearenaApplication {
+class GameArenaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GamearenaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameArenaApplication.class, args);
+    }
 
 }
