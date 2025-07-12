1546051829c1fab954c7acae48057e7aa9b48835
Gladyson Rhuan <gladysonr04@gmail.com>
2025-07-12 17:24:52 -0300

    Removendo imports

--- BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/security/UserAuthenticationFilter.java
+++ BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/security/UserAuthenticationFilter.java
@@ -1,24 +1,18 @@
 package com.unifucamp.gamearena.security;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-
 import com.unifucamp.gamearena.entity.User;
 import com.unifucamp.gamearena.repository.UserRepository;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 public class UserAuthenticationFilter  extends OncePerRequestFilter {
