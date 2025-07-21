723d3642f3f94998ce59506e1d424dc8f5286076
Gladyson Rhuan <gladysonr04@gmail.com>
2025-07-21 20:39:14 -0300

    Acesso restrito ao perfil ADMIN para operações de listagem e exclusão de usuários

--- BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/controller/UserController.java
+++ BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/controller/UserController.java
@@ -1,19 +1,21 @@
 package com.unifucamp.gamearena.controller;
 
-import com.unifucamp.gamearena.dto.CreateUserDto;
-import com.unifucamp.gamearena.dto.LoginUserDto;
-import com.unifucamp.gamearena.dto.RecoveryJwtTokenDto;
+import com.unifucamp.gamearena.controller.dto.CreateUserDto;
+import com.unifucamp.gamearena.controller.dto.LoginUserDto;
+import com.unifucamp.gamearena.controller.dto.RecoveryJwtTokenDto;
+import com.unifucamp.gamearena.entity.User;
 import com.unifucamp.gamearena.service.UserService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService  userService;
@@ -23,16 +25,31 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<RecoveryJwtTokenDto> authenticateUser(@RequestBody LoginUserDto loginUserDto) {
+    public ResponseEntity<RecoveryJwtTokenDto> authenticateUser(@RequestBody @Valid LoginUserDto loginUserDto) {
         RecoveryJwtTokenDto token = userService.authenticateUser(loginUserDto);
         return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/register")
-    public ResponseEntity<Void> createUser(@RequestBody CreateUserDto createUserDto) {
+    public ResponseEntity<Void> createUser(@RequestBody @Valid CreateUserDto createUserDto) {
         userService.createUser(createUserDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<User>>  getAllUsers() {
+        return ResponseEntity.ok(userService.listUsers());
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
 
--- BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/service/UserService.java
+++ BackEnd_GameArena/src/main/java/com/unifucamp/gamearena/service/UserService.java
@@ -1,20 +1,27 @@
 package com.unifucamp.gamearena.service;
 
-import com.unifucamp.gamearena.dto.CreateUserDto;
-import com.unifucamp.gamearena.dto.LoginUserDto;
-import com.unifucamp.gamearena.dto.RecoveryJwtTokenDto;
+import com.unifucamp.gamearena.controller.dto.CreateUserDto;
+import com.unifucamp.gamearena.controller.dto.LoginUserDto;
+import com.unifucamp.gamearena.controller.dto.RecoveryJwtTokenDto;
 import com.unifucamp.gamearena.entity.Role;
 import com.unifucamp.gamearena.entity.User;
+import com.unifucamp.gamearena.exception.GameArenaException;
+import com.unifucamp.gamearena.exception.UserDataAlreadyExistsException;
+import com.unifucamp.gamearena.exception.UserNotExistsException;
 import com.unifucamp.gamearena.repository.UserRepository;
 import com.unifucamp.gamearena.security.JwtTokenService;
 import com.unifucamp.gamearena.security.SecurityConfiguration;
 import com.unifucamp.gamearena.security.UserDetailsImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,20 +30,24 @@ public class UserService {
     private final JwtTokenService jwtTokenService;
     private final AuthenticationManager authenticationManager;
     private final SecurityConfiguration securityConfiguration;
+    private final RoleService roleService;
+    @Value("${admin.email}")
+    private String adminEmail;
 
-    public UserService(UserRepository userRepository, JwtTokenService jwtTokenService, AuthenticationManager authenticationManager, SecurityConfiguration securityConfiguration) {
+    public UserService(UserRepository userRepository, JwtTokenService jwtTokenService, AuthenticationManager authenticationManager, SecurityConfiguration securityConfiguration, RoleService roleService) {
         this.userRepository = userRepository;
         this.jwtTokenService = jwtTokenService;
         this.authenticationManager = authenticationManager;
         this.securityConfiguration = securityConfiguration;
+        this.roleService = roleService;
     }
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     public RecoveryJwtTokenDto authenticateUser(LoginUserDto loginUserDto) {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                 new UsernamePasswordAuthenticationToken(loginUserDto.email(), loginUserDto.password());
-
         Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
-
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
         return new RecoveryJwtTokenDto(jwtTokenService.generateToken(userDetails));
@@ -44,13 +55,46 @@ public class UserService {
 
     public void createUser(CreateUserDto createUserDto) {
 
-        User newUser = User.builder()
-                .email(createUserDto.email())
-                .password(securityConfiguration.passwordEncoder().encode(createUserDto.password()))
-                .roles(List.of(Role.builder().name(createUserDto.role()).build()))
-                .build();
+        Optional<User> user = userRepository.findByEmail(createUserDto.email());
+
+        if (user.isPresent()){
+            throw new UserDataAlreadyExistsException("Esse email já está cadastrado");
+        }
+
+        String password = securityConfiguration.passwordEncoder().encode(createUserDto.password());
+
+        Role role = roleService.buscarRole(createUserDto.role());
+
+        User newUser = new User(
+                createUserDto.email(),
+                password,
+                List.of(role)
+        );
 
         userRepository.save(newUser);
+
+        log.info("Novo usuario cadastrado com sucesso");
+    }
+
+    public List<User> listUsers() {
+
+         return userRepository.findAll();
+    }
+
+    public void deleteUser(String id) {
+
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()){
+            throw new UserNotExistsException();
+        }
+
+        if (user.get().getEmail().equals(adminEmail)){
+            throw new GameArenaException();
+        }
+
+        userRepository.deleteById(id);
+        log.info("Usuário deletado: {}", id);
     }
 
 }
